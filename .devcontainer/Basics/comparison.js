5a90c75db224cf53414b761809063da6cbc9d3f3
Arman Ali <88931306+armanalirahul@users.noreply.github.com>
2023-12-23 11:16:00 +0000

    comparison

---
+++ .devcontainer/Basics/comparison.js
@@ -0,0 +1,28 @@
+//basids
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//this type of conversion we have to avoid for readable 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+//this type end
+
+// ===  this operator not only value check its check also value data type
+
+console.log("2" === 2); //false coz value is same but data type is different
+
+

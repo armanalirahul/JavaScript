b097e0f462edd2c88456d1dc25761a228b48b722
Arman Ali <88931306+armanalirahul@users.noreply.github.com>
2023-12-28 20:47:05 +0000

    Date Object and formates of dates

---
+++ .devcontainer/Basics/Date.js
@@ -0,0 +1,18 @@
+
+let myDate = new Date();
+
+//some formates of dates
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+//let myCreateDate = new Date(2023, 0, 23);
+//console.log(myCreateDate.toDateString());
+
+//let myCreateDate = new Date(2023, 0, 23, 5, 3);
+//console.log(myCreateDate.toLocaleString());
+
+let myCreateDate = new Date("01-14-2023");
+console.log(myCreateDate.toLocaleString());
+
+

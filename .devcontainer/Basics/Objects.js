76115b9a8814298f81cc484dde2eeac85cff2ec3
Arman Ali <88931306+armanalirahul@users.noreply.github.com>
2024-01-15 18:41:19 +0000

    ObjectLearning

---
+++ .devcontainer/Basics/Objects.js
@@ -0,0 +1,35 @@
+//Singleton
+//Object.create
+
+
+//object literals
+
+const mySum = Symbol("Key6")
+
+const JsUser = {
+    name: "Arman",  //key value pair - key is by default String like this - name 
+    "full name": "Arman Ali",
+    [mySum]: "key1", // use of symbol key
+    age: 18,
+    location: "Jaipur",
+    email: "arman.ali@tcs.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySum])
+
+JsUser.email = "armanali@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "armanali@microsoft.com"
+//console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello Arman Ali")
+    console.log(`Hello Arman Ali, ${this.name}`)
+}
+
+console.log(JsUser.greeting())
